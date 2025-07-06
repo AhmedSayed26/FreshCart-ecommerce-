d19139940ae288295ba73c91db34b6fe9ad41692
ahmedsayed270 <22ahmed23sayed24@gmail.com>
2025-07-06 16:32:46 +0300

    finsh product details

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,12 +1,21 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Loading from "../../Components/loading/Loading";
+import { CartContext } from "./../../Components/Context/Cart.context";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
+import Card from "./../../Components/Card/Card";
+import ReactImageGallery from "react-image-gallery";
+// import "react-image-gallery/styles/css/image-gallery.css";
 
 export default function ProductDetails() {
+  const { addTocart } = useContext(CartContext);
   const { id } = useParams();
   console.log(id);
   const [productdetail, setproductdetail] = useState(null);
+  const [relatedProduct, setrelatedProduct] = useState(null);
   async function getProductDetails() {
     const option = {
       url: `https://ecommerce.routemisr.com/api/v1/products/${id}`,
@@ -15,34 +24,83 @@ export default function ProductDetails() {
     const { data } = await axios.request(option);
     setproductdetail(data.data);
   }
+
+  async function getSpacificProduct() {
+    const options = {
+      url: `https://ecommerce.routemisr.com/api/v1/products?category[in]=${productdetail.category._id}`,
+      method: "Get",
+    };
+    const { data } = await axios.request(options);
+    console.log(data);
+    setrelatedProduct(data.data);
+  }
+
   useEffect(() => {
     getProductDetails();
   }, [id]);
+  useEffect(() => {
+    if (productdetail) {
+      getSpacificProduct();
+    }
+  }, [productdetail]);
   return (
     <>
       {productdetail ? (
-        <div className="grid grid-cols-12">
-          <div className="col-span-4">
-            <img className="w-ful" src={productdetail.imageCover} alt="" />
-          </div>
-          <div className="col-span-8 py-5 space-y-5">
-            <div>
-              <h2 className="text-xl"> {productdetail.title}</h2>
-              <h2 className="text-xl font-semibold text-mainColor">
-                {productdetail.category.name}
-              </h2>
+        <>
+          <div className="grid grid-cols-12 gap-5 my-10">
+            <div className="col-span-4">
+              {/* <img className="w-ful" src={productdetail.imageCover} alt="" /> */}
+              <ReactImageGallery
+                items={productdetail.images.map((image) => {
+                  return { original: image, thumbnail: image };
+                })}
+              ></ReactImageGallery>
             </div>
-            <p>{productdetail.description}</p>
-            <div className="flex items-center justify-between">
-              <h4>{productdetail.price} EGP</h4>
-              <h4>
-                {productdetail.ratingAverage}
-                <i className="fa-solid fa-star text-yellow-500"></i>
-              </h4>
+            <div className="col-span-8 py-5 space-y-5">
+              <div>
+                <h2 className="text-xl"> {productdetail.title}</h2>
+                <h2 className="text-xl font-semibold text-mainColor">
+                  {productdetail.category.name}
+                </h2>
+              </div>
+              <p>{productdetail.description}</p>
+              <div className="flex items-center justify-between">
+                <h4>{productdetail.price} EGP</h4>
+                <h4 className="flex items-center gap-2">
+                  <p>{productdetail.ratingsAverage}</p>
+                  <i className="fa-solid fa-star text-yellow-500"></i>
+                </h4>
+              </div>
+              <button
+                onClick={() => {
+                  addTocart(id);
+                }}
+                className="btn w-full"
+              >
+                Add to Cart
+              </button>
             </div>
-            <button className="btn w-full">Add to Cart</button>
           </div>
-        </div>
+          <div className="my-10">
+            <h2 className="text-2xl font-semibold my-5">Related Product :</h2>
+            <Swiper
+              slidesPerView={6}
+              spaceBetween={3}
+              loop={false}
+              autoplay={{
+                delay: 500,
+                disableOnInteraction: false,
+                pauseOnMouseEnter: true,
+              }}
+            >
+              {relatedProduct?.map((product) => (
+                <SwiperSlide key={product.id}>
+                  <Card productInfo={product}></Card>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </>
       ) : (
         <Loading></Loading>
       )}
